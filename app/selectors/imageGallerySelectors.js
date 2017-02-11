aeed3f0a606907f7249c79f3ea78e9f53ad952f3
Lance Newby <lnewby@gmail.com>
2017-02-10 16:08:15 -0800

    add more edge case test to selectors

--- app/selectors/imageGallerySelectors.js
+++ app/selectors/imageGallerySelectors.js
@@ -1,3 +1,3 @@
-export const getImage = (state = {}, id) => state.byId[id];
+export const getImage = (state = {}, id) => state.byId[id] || {};
 
 export const getAllImages = (state) => [...state.byId];
--- tests/imageGallerySelectors.tests.js
+++ tests/imageGallerySelectors.tests.js
@@ -17,11 +17,33 @@ test('selectors - getImage :: return an image object from state',
   }
 );
 
+test('selectors - getImage :: return an empty object if no images exist',
+  ({ deepEqual, end }) => {
+    const initialState = getMockState.withoutImages();
+    const actualState = selectors.getImage(initialState, '3');
+    const expectedState = {};
+
+    deepEqual(actualState, expectedState);
+    end();
+  }
+);
+
 test('selectors - getAllImages :: return an array of all image objects from state',
   ({ deepEqual, end }) => {
     const initialState = getMockState.withManyImages();
     const actualState = selectors.getAllImages(initialState);
     const expectedState = [...initialState.byId];
+
+    deepEqual(actualState, expectedState);
+    end();
+  }
+);
+
+test('selectors - getAllImages :: return an empty array if no images exist',
+  ({ deepEqual, end }) => {
+    const initialState = getMockState.withoutImages();
+    const actualState = selectors.getAllImages(initialState);
+    const expectedState = [];
 
     deepEqual(actualState, expectedState);
     end();
